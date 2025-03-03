1b4301e8b35593d5f20cc3cf21f171bcdf60f387
Maksym Omelkov <omelkov85@gmail.com>
2025-03-03 15:29:41 +0200

    Lockdown cafe (1.0)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import VaccineError, NotWearingMaskError
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    count_masks = 0
+    all_vaccinated = True
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            all_vaccinated = False
+        except NotWearingMaskError:
+            count_masks += 1
+
+    if not all_vaccinated:
+        return ("All friends should be vaccinated")
+    elif count_masks > 0:
+        return (f"Friends should buy {count_masks} masks")
+    else:
+        return f"Friends can go to {cafe.name}"
